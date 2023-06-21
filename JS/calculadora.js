6e4b227cb44b435921e497c7d5cbeeb94d6b0799
Maximiliano <djmaxirey@gmail.com>
2023-06-21 17:17:09 -0300

    calculadora JS

---
+++ JS/calculadora.js
@@ -0,0 +1,41 @@
+function calculadora(button, display) {
+    switch(button.innerHTML) {
+        case "C":
+            borrar(display)
+            break
+        case "=":
+            calcular(display)
+            break
+        default:
+            actualizar(display, button)
+            break
+    }
+}
+
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display, button) {
+    if (display.innerHTML == 0) {
+        display.innerHTML = " "
+    }
+    display.innerHTML += button.innerHTML
+}
+
+function borrar(display){
+    display.innerHTML = 0
+}
+
+window.addEventListener("load", ()=> {
+    const display = document.querySelector(".calculator-display")
+    const keypadButtons = document.getElementsByClassName("keypad-buttons")
+
+    const keypadButtonsArray = Array.from(keypadButtons)
+
+    keypadButtonsArray.forEach( (button) => {
+        button.addEventListener("click", () => {
+            calculadora(button, display)
+        })
+    })
+})
